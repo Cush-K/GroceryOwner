cec0e2a750d626896a729b5b46660530ef58cd09
Mbuthiasakara1 <mbuthiaryne@gmail.com>
2024-08-13 21:07:15 +0300

    updated pictures couldn't load

--- src/components/ItemCard.js
+++ src/components/ItemCard.js
@@ -2,29 +2,26 @@ import React from "react";
 import { useState } from "react";
 import { useParams } from "react-router-dom";
 
+function ItemCard() {
+  const params = useParams();
+  const [items, setItems] = useState({});
+  const itemId = params.id;
 
-function ItemCard(){
+  fetch(`http://localhost:4000/products/${itemId}`)
+    .then((resp) => resp.json())
+    .then((data) => setItems(data))
+    .catch((error) => console.error(error));
 
-    const params = useParams();
-    const [items, setItems] = useState({})
-    const itemId = params.id
-
-       
-    fetch(`http://localhost:4000/products/${itemId}`)
-    .then(resp => resp.json())
-    .then(data => setItems(data))
-    .catch(error => console.error(error))
-
-    return(
-        <div className="item-card">
-            <h1>{items.name}</h1>
-            <img src={items.image} alt="product" />
-            <h2>Price: {items.price}</h2>
-            <p>Description: {items.description}</p>
-            <p>Category: {items.category}</p>
-            <p>Quantity in Stock: {items.quantity}</p>
-        </div>
-    )
+  return (
+    <div className="item-card">
+      <h1>{items.name}</h1>
+      <img src={items.image} alt="product" />
+      <h2>Price: {items.price}</h2>
+      <p>Description: {items.description}</p>
+      <p>Category: {items.category}</p>
+      <p>Quantity in Stock: {items.quantity}</p>
+    </div>
+  );
 }
 
 export default ItemCard;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,34 +1,27 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import ItemList from "../components/itemList";
 
 function Home() {
+  const [products, setProducts] = useState([]);
 
-    const [products, setProducts] = useState([]);
-
-    useEffect(()=>{
-      fetch(`http://localhost:4000/products`)
-      .then(resp => resp.json())
-      .then(data => setProducts(data))
-      .catch(error => console.error(error))
-    }, [])
+  useEffect(() => {
+    fetch(`http://localhost:4000/products`)
+      .then((resp) => resp.json())
+      .then((data) => setProducts(data))
+      .catch((error) => console.error(error));
+  }, []);
 
   return (
     <div className="App">
-
-      {
-        products.map(product => (
-          <ItemList 
-            key={product.id}  
-            id ={product.id}
-            name={product.name} 
-            image={product.image} 
-            price={product.price} 
-          />
-        ))
-      }
-
-      
-      
+      {products.map((product) => (
+        <ItemList
+          key={product.id}
+          id={product.id}
+          name={product.name}
+          image={product.image}
+          price={product.price}
+        />
+      ))}
     </div>
   );
 }
